f7ed3e7559938805734fd1a76fc880d133668795
Suxto <suxton@icloud.com>
2022-10-24 21:45:07 +0800

    test

---
+++ resorces/roles/info.js
@@ -0,0 +1,29 @@
+var roles = [{
+  roleName: "guu",
+  id: 0
+}, {
+  roleName: "abab",
+  id: 1
+}, {
+  roleName: "dfd",
+  id: 2
+}, {
+  roleName: "fdf",
+  id: 3
+}, {
+  roleName: "dfu",
+  id: 4
+}, {
+  roleName: "ngu",
+  id: 5
+}, {
+  roleName: "gbf",
+  id: 6
+}, {
+  roleName: "wou",
+  id: 7
+}];
+
+module.exports = {
+  roles: roles
+};
---
+++ resorces/texts/missions.js
@@ -0,0 +1,72 @@
+var missi = [{
+  id: 0,
+  missionShown: 3,
+  missionList: [{
+    text: "前往侍女馆找梁老师",
+    status: "block"
+  }, {
+    text: "接下梁老师手上的狗链",
+    status: "block"
+  }, {
+    text: "套上狗链，成为梁老师的狗",
+    status: "block"
+  }, {
+    text: "jogggjojo",
+    status: "none"
+  }, {
+    text: "jojogggdjo",
+    status: "none"
+  }]
+}, {
+  id: 1,
+  missionShown: 3,
+  missionList: [{
+    text: "前往侍女馆找梁老师",
+    status: "block"
+  }, {
+    text: "接下梁老师手上的狗链",
+    status: "block"
+  }, {
+    text: "套上狗链，成为梁老师的狗",
+    status: "block"
+  }, {
+    text: "jogggjojo",
+    status: "block"
+  }, {
+    text: "jojogggdjo",
+    status: "none"
+  }]
+}];
+
+var pla = [
+  {
+    id: 1, status: false,
+    x: "140rpx", y: "1155rpx",
+    placeName: "西门",
+    text: "唐宝应元年，肃宗病重，皇后张氏妄图联合越王李系废黜太子，太子李豫在宦官李辅国、程元振的拥护下掌握先机，软禁叛臣，后肃宗崩逝，太子李豫即位，改年号为宝应，重用宦官李辅国，命其掌管御马监，存兵符，统摄宫中守卫。宝应二年，郭子仪等率众平息安史之乱，代宗采纳仆固怀恩分授原安史旧将镇守河朔的提议，是为藩镇割据之始。广德元年，大太监李辅国专横跋扈、骄奢淫逸，代宗和宰相元载设计处死李辅国，拿回被其控制的兵权。代宗提拔程元振统领禁军，代宗优宠宦官，宦官奉旨出使，代宗从不禁止其所求取，以致一时间北司各监太监“公求赂遗，无所忌惮”古人云“楚王好细腰，宫女多饿死。”皇上优宠宦臣的风气传到民间，一时间不少穷苦百姓选择将自家孩子送入宫，多换取些银钱，广德到大历年间，北司风光无两，疯狂圈占银钱与土地，有权势的宦官的私产竟是朝官的数倍......\n 请向NPC寻求更多线索，并在下面的框中输入NPC提供的密码:",
+    passwd: 1234
+  }, {
+    id: 2, status: false,
+    x: "100rpx", y: "790rpx",
+    placeName: "凤鸣学堂",
+    text: "ababab",
+    passwd: 1234
+  }, {
+    id: 3, status: false,
+    x: "75rpx", y: "490rpx",
+    placeName: "陆羽茶舍",
+    text: "ababab",
+    passwd: 1234
+  }, {
+    id: 4, status: false,
+    x: "460rpx", y: "190rpx",
+    placeName: "竹里馆",
+    text: "ababab",
+    passwd: 1234
+  }
+];
+
+module.exports = {
+  missions: missi,
+  places: pla
+}
